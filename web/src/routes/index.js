a754460b0c5d50e05402268f489af26352756f1d
Haarlem N <haarlemn@gmail.com>
2020-10-27 22:28:30 -0300

    route configuration

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import NewSpot from './pages/NewSpot';
+import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+import NewSpot from '../pages/NewSpot';
 
 export default function Routes() {
   return (

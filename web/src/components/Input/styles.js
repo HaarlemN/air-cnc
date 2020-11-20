f0a892f3b55cb1c1e6371904c1ae26ecf7dbd6de
Haarlem N <haarlemn@gmail.com>
2020-11-20 14:39:27 -0300

    input responsiveness

--- web/src/components/Input/styles.js
+++ web/src/components/Input/styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+
+  width: 100%;
 `;
 
 export const Label = styled.label`
@@ -13,11 +15,11 @@ export const Label = styled.label`
 `;
 
 export const MyInput = styled.input`
-  margin-bottom: 20px;
+  margin-bottom: 2.2rem;
   border: 1px solid #ddd;
   border-radius: 2px;
   height: 4.8rem;
-  padding: 0 15px;
+  padding: 0 1rem;
   font-size: 1.6rem;
   transition: border 0.3s;
 
